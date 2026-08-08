12ef0a2632e208012e5d70f66e4fba4b8f96dbbd
tsnaketech <34743212+tsnaketech@users.noreply.github.com>
2026-08-08 03:05:42 +0200

    fix: skip credential assets during build

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,14 @@ function copyNodeIcons() {
 	return src('nodes/**/*.{png,svg}').pipe(dest('dist/nodes'));
 }
 
-function copyCredentialAssets() {
-	return src('credentials/**/*.{png,svg}', { base: 'credentials', allowEmpty: true }).pipe(
-		dest('dist/credentials'),
-	);
-}
+// function copyCredentialAssets() {
+// 	return src('credentials/**/*.{png,svg}', { base: 'credentials', allowEmpty: true }).pipe(
+// 		dest('dist/credentials'),
+// 	);
+// }
 
 function copyIcons() {
 	return src('icons/**/*.{png,svg}', { base: 'icons' }).pipe(dest('dist/icons'));
 }
 
-exports['build:icons'] = parallel(copyNodeIcons, copyCredentialAssets, copyIcons);
+exports['build:icons'] = parallel(copyNodeIcons, copyIcons);
